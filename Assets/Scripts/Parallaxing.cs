28803a9908e7a358cdd9eece4347da361c04acf9
Nurettin Onur YAKIT <onuryakit@gmail.com>
2020-05-24 19:33:41 +0200

    Checkpoint - Parallax

---
+++ Assets/Scripts/Parallaxing.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallaxing : MonoBehaviour
+{
+    public Transform[] backgrounds;
+    private float[] parallaxScales;
+    public float smoothing = 1f;
+
+    public Transform camera;
+    private Vector3 previousCamPosition;
+
+    void awake()
+    {
+        camera = Camera.main.transform;
+    }
+
+    void Start()
+    {
+        previousCamPosition = camera.position;
+        parallaxScales = new float[backgrounds.Length];
+
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            parallaxScales[i] = backgrounds[i].position.z * -1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            float parallax = (previousCamPosition.x - camera.position.x) * parallaxScales[i];
+            float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+        }
+
+        previousCamPosition = camera.position;
+    }
+}
