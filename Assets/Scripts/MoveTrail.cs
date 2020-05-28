46e08ba97a3226c1c2f681038c1d5a90e102b1d1
Nurettin Onur YAKIT <onuryakit@gmail.com>
2020-05-28 22:48:55 +0200

    Checkpoint shootin
    Shooting

---
+++ Assets/Scripts/MoveTrail.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTrail : MonoBehaviour
+{
+
+    private int moveSpeed = 200;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right*Time.deltaTime * moveSpeed);
+        Destroy(gameObject,1);
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,9 +6,15 @@ public class Weapon : MonoBehaviour
 {
     public float fireRate = 0;
     public float damage = 10;
+    public float effectSpawnRate=10;
     public LayerMask whatToHit;
 
+    public Transform bulletTrailPrefab;
+    public Transform muzzleFlashPrefab;
+    
     float timeToFire = 0;
+    float timeToSpawnEffect =0;
+    
     Transform firePoint;
 
     // Start is called before the first frame update
@@ -35,7 +41,7 @@ public class Weapon : MonoBehaviour
         }
         else
         {
-            if (Input.GetButtonDown("Fire1") && Time.time > timeToFire)
+            if (Input.GetButton("Fire1") && Time.time > timeToFire)
             {
                 timeToFire = Time.time + 1 / fireRate;
                 Shoot();
@@ -45,16 +51,32 @@ public class Weapon : MonoBehaviour
 
     void Shoot()
     {
-        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
-        Vector2 firePointPosition = new Vector2(firePoint.position.x,firePoint.position.y);
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
 
-        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition-firePointPosition,100,whatToHit);
-
-        Debug.DrawLine(firePointPosition,(mousePosition-firePointPosition)*100,Color.cyan);
-
-        if(hit.collider!=null){
-            Debug.DrawLine(firePointPosition,hit.point,Color.red);
-            Debug.Log("We hit "+hit.collider.name+" and did damage "+damage);
+        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, whatToHit);
+        if (Time.time >= timeToSpawnEffect)
+        {
+            Effect();
+            timeToSpawnEffect = Time.time + 1/effectSpawnRate;
         }
+
+        Debug.DrawLine(firePointPosition, (mousePosition - firePointPosition) * 100, Color.cyan);
+
+        if (hit.collider != null)
+        {
+            Debug.DrawLine(firePointPosition, hit.point, Color.red);
+            Debug.Log("We hit " + hit.collider.name + " and did damage " + damage);
+        }
+    }
+
+    void Effect()
+    {
+        Instantiate(bulletTrailPrefab, firePoint.position, firePoint.rotation);
+        Transform muzzleFlashClone = Instantiate(muzzleFlashPrefab, firePoint.position, firePoint.rotation) as Transform;
+        muzzleFlashClone.parent = firePoint;
+        float size = Random.Range(0.6f,0.9f);
+        muzzleFlashClone.localScale = new Vector3(size,size,size);
+        Destroy(muzzleFlashClone.gameObject,0.02f);
     }
 }
