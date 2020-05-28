f4741578044629010d56ff5543c442148560e86a
Nurettin Onur YAKIT <onuryakit@gmail.com>
2020-05-28 21:13:15 +0200

    Checkpoint - Fire Nation
    Long ago, the four nations lived together in harmony...

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float fireRate = 0;
+    public float damage = 10;
+    public LayerMask whatToHit;
+
+    float timeToFire = 0;
+    Transform firePoint;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        firePoint = transform.Find("FirePoint");
+        if (firePoint == null)
+        {
+            Debug.LogError("firePoint null!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Shoot();
+        if (fireRate == 0)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+        else
+        {
+            if (Input.GetButtonDown("Fire1") && Time.time > timeToFire)
+            {
+                timeToFire = Time.time + 1 / fireRate;
+                Shoot();
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 firePointPosition = new Vector2(firePoint.position.x,firePoint.position.y);
+
+        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition-firePointPosition,100,whatToHit);
+
+        Debug.DrawLine(firePointPosition,(mousePosition-firePointPosition)*100,Color.cyan);
+
+        if(hit.collider!=null){
+            Debug.DrawLine(firePointPosition,hit.point,Color.red);
+            Debug.Log("We hit "+hit.collider.name+" and did damage "+damage);
+        }
+    }
+}
