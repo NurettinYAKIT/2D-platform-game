4ab3b6ceee6c2e6d0c8cd99e9f0cde74772c8314
Nurettin Onur YAKIT <onuryakit@gmail.com>
2020-06-03 23:09:09 +0200

    Enemy Health

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,20 +7,47 @@ public class Enemy : MonoBehaviour
     [System.Serializable]
     public class EnemyStats
     {
-        public int health = 100;
+        public int maxHealth = 100;
+        private int _currentHealth;
+        public int currentHealth
+        {
+            get { return _currentHealth; }
+            set { _currentHealth = Mathf.Clamp(value, 0, maxHealth); }
+        }
+
+        public void Init()
+        {
+            currentHealth = maxHealth;
+        }
+
     }
 
     public EnemyStats stats = new EnemyStats();
+    [Header("Optional:")]
+    [SerializeField]
+    private StatusIndicator statusIndicator;
+    private void Start()
+    {
+        stats.Init();
 
+        if (statusIndicator != null)
+        {
+            statusIndicator.SetHealth(stats.currentHealth, stats.maxHealth);
+        }
+    }
     public void Damage(int damage)
     {
         Debug.Log("Enemy Damaged " + damage);
 
-        stats.health -= damage;
-        if (stats.health <= 0)
+        stats.currentHealth -= damage;
+        if (stats.currentHealth <= 0)
         {
             Debug.Log("Enemy Killed");
             GameManager.KillEnemy(this);
+        }
+        if (statusIndicator != null)
+        {
+            statusIndicator.SetHealth(stats.currentHealth, stats.maxHealth);
         }
     }
 
---
+++ Assets/Scripts/StatusIndicator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusIndicator : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform healthBarRect;
+    [SerializeField]
+    private Text healthText;
+
+    private void Start()
+    {
+        if (healthBarRect == null)
+        {
+            Debug.LogError("STATUS INDICATOR : No Health Bar available");
+        }
+        if (healthText == null)
+        {
+            Debug.LogError("STATUS INDICATOR : No Health Text available");
+        }
+    }
+
+    public void SetHealth(int _current, int _max)
+    {
+        float _value = (float)_current / _max;
+        healthBarRect.localScale = new Vector3(_value, healthBarRect.localScale.y, healthBarRect.localScale.z);
+
+        healthText.text = _current + "/" + _max + " HP";
+
+    }
+}
