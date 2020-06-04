f6fb6fe3bfe0b73e541caad9d832123d751ebc00
Nurettin Onur YAKIT <onuryakit@gmail.com>
2020-06-04 22:27:39 +0200

    test

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+
+    public enum SpawnState { SPAWNING, WAITING, COUNTING }
+
+    [System.Serializable]
+    public class Wave
+    {
+
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+    public float timeBetweenWaves = 5f;
+    public float waveCountDown;
+
+    private float searchCountDown = 1f;
+
+    public SpawnState state = SpawnState.COUNTING;
+
+    private void Start()
+    {
+        waveCountDown = timeBetweenWaves;
+    }
+
+    private void Update()
+    {
+
+        if (state == SpawnState.WAITING)
+        {
+            if (!IsEnemyAlive())
+            {
+                //Begin a new round
+                Debug.Log("Starting the new Round!");
+                return;
+            }
+            else
+            {
+                return;
+            }
+        }
+        if (waveCountDown <= 0)
+        {
+            if (state != SpawnState.SPAWNING)
+            {
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        }
+        else
+        {
+            waveCountDown -= Time.deltaTime;
+        }
+    }
+
+    private IEnumerator SpawnWave(Wave _wave)
+    {
+        state = SpawnState.SPAWNING;
+
+        for (int i = 0; i < _wave.count; i++)
+        {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds(1f / _wave.rate);
+        }
+
+        state = SpawnState.WAITING;
+        yield break;
+    }
+
+    private void SpawnEnemy(Transform _enemy)
+    {
+        Debug.Log("Spawning enemy " + _enemy.name);
+        Instantiate(_enemy, transform.position, transform.rotation);
+    }
+
+    bool IsEnemyAlive()
+    {
+        searchCountDown -= Time.deltaTime;
+
+        if (searchCountDown <= 0f)
+        {
+            searchCountDown = 1f;
+            if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+            {
+                Debug.Log("No emeny left.");
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
