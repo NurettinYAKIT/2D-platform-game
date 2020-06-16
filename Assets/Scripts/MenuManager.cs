401bb448af65f223c6ffd3bf2bf3b2244348786c
Nurettin Onur YAKIT <onuryakit@gmail.com>
2020-06-16 22:30:37 +0200

    Main Menu
    Now we have a menu with play & quit options.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("APPLICATION QUIT!");
+        Application.Quit();
+    }
+}
